b55339bc1cf81d73fbd4c9d936881db43e9dfca9
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-06-13 17:24:22 +0530

    frontend commit
    frontend commit

---
+++ frontend/src/app/attendance/page.jsx
@@ -0,0 +1,10 @@
+// import Attendance from "@/Components/Attendance";
+import DataTableAttendance from "@/Components/DataTableAttendance";
+
+export default function attendance() {
+  return (
+    <div className="m-full h-lvh flex items-center justify-center">
+      <DataTableAttendance />
+    </div>
+  );
+}
