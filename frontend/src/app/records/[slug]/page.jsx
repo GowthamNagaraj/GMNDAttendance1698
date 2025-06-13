95d882a4f747d771ce32640350800296cb8180a4
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-06-13 20:39:46 +0530

    testing

--- frontend/src/app/records/[slug]/page.jsx
+++ frontend/src/app/records/[slug]/page.jsx
@@ -2,7 +2,7 @@ import DataTableAttendance from "@/Components/DataTableAttendance";
 
 export default async function AttendancePage({ params }) {
   const slug = params.slug;
-  console.log("slug:", slug);
+  console.log("slug: ", slug);
 
   return (
     <div className="w-full h-lvh flex items-center justify-center">
